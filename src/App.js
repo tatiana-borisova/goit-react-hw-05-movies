e5110a5672ee4c858d3f34039bcee4ae198a0d07
tatiana-borisova <0102tb@gmail.com>
2021-12-10 15:38:56 +0100

    loader is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import Loader from 'react-loader-spinner';
 import Section from './components/Section';
 import AppBar from './components/AppBar';
 import HomePage from './components/HomePage';
 import MoviesPage from './components/MoviesPage';
-import MovieDetailsPage from './components/MovieDetailsPage';
-import Cast from './components/Cast';
-import Reviews from './components/Reviews';
+
+const MovieDetailsPage = lazy(() => import('./components/MovieDetailsPage'));
+const Cast = lazy(() => import('./components/Cast'));
+const Reviews = lazy(() => import('./components/Reviews'));
+const loader = <Loader color="#0f548d" style={{ textAlign: 'center' }} />;
 
 export default function App() {
   return (
@@ -16,9 +20,30 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<MoviesPage />} />
-        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
+        <Route
+          path="/movies/:movieId"
+          element={
+            <Suspense fallback={loader}>
+              <MovieDetailsPage />
+            </Suspense>
+          }
+        >
+          <Route
+            path="cast"
+            element={
+              <Suspense fallback={loader}>
+                <Cast />
+              </Suspense>
+            }
+          />
+          <Route
+            path="reviews"
+            element={
+              <Suspense fallback={loader}>
+                <Reviews />
+              </Suspense>
+            }
+          />
         </Route>
 
         <Route path="*" element={<HomePage />} />
